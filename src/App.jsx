4df00864926a19e8350fc5e985e4f59f653a8c4b
EnricoGeno <enricogeno@libero.it>
2025-04-02 15:26:28 +0200

    fix updateProductQuantity; Milestone_3

--- src/App.jsx
+++ src/App.jsx
@@ -25,18 +25,47 @@ const products = [
 // Quantità
 
 // Obiettivo: L’utente può aggiungere prodotti al carrello e vedere una lista dei prodotti aggiunti.
+
+// Milestone 3: Modificare il carrello
+// Al click successivo del bottone "Aggiungi al carrello", se il prodotto è già presente:
+// Usa una funzione updateProductQuantity per incrementare la proprietà quantity del prodotto esistente.
+// Per ogni prodotto nel carrello, aggiungi un bottone "Rimuovi dal carrello":
+// Al click, usa una funzione removeFromCart per rimuovere il prodotto dal carrello.
+// Sotto alla lista del carrello, mostra il totale da pagare:
+// Calcola il totale moltiplicando il prezzo per la quantità di ogni prodotto e somma tutti i risultati.
+// Obiettivo: Gestire l’aggiunta, la rimozione e il calcolo del totale del carrello in modo dinamico.
+
 function App() {
 
   const [addedProducts, setAddedProducts] = useState([])
 
-  const addToCart = (i) => {
-    const product = products[i];
+  const addToCart = (product) => {
     const isInCart = addedProducts.some((p) => p.name === product.name)
     if (!isInCart) {
       setAddedProducts([...addedProducts, { ...product, quantity: 1 }])
-    } else { return }
+    } else {
+      updateProductQuantity(product)
+    }
+  };
 
+
+  const updateProductQuantity = (product) => {
+    setAddedProducts((curr) => curr.map((p) => {
+      if (p.name === product.name) {
+        return { ...p, quantity: p.quantity + 1 }
+      }
+      return p;
+    }))
   }
+
+  const removeFromCart = (product) => {
+    setAddedProducts(addedProducts.filter((p) => p.name !== product.name))
+  }
+
+  const priceArray = addedProducts.map((p) => parseFloat(p.price * p.quantity))
+  const totalPrice = priceArray.reduce((acc, curr) => {
+    return acc + parseFloat(curr)
+  }, 0)
 
   return (
     <>
@@ -45,17 +74,19 @@ function App() {
         {products.map((p, i) => (
           <li key={i}>
             <p>{p.name} ({p.price}€)</p>
-            <button onClick={() => addToCart(i)}> Aggiungi al carrello</button>
+            <button onClick={() => addToCart(p)}> Aggiungi al carrello</button>
+            <button onClick={() => removeFromCart(p)}>Rimuovi dal carrello</button>
           </li>
         ))}
       </ul>
       <h2>Carrello</h2>
       {addedProducts.length >= 1 ? addedProducts.map((p, i) => (
         <li key={i}>
-          <p>{p.name} ({p.price}€) {p.quantity}</p>
+          <p>{p.name} ({p.price}€) Quantità: {p.quantity}</p>
         </li>
       ))
         : <p>Il carrello è vuoto</p>}
+      <p>Totale: {totalPrice.toFixed(2)}€</p>
     </>
   )
 }
